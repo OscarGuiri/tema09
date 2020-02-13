d99e751f595d7e70e2b8f4aff6c2ea9f7fcd2909
OscarGuiri <oscar.morton.a@ies1xabia.org>
2020-02-13 19:20:54 +0100

    Ejer 11 a medias

--- src/main/java/com/oscarmorton/ejer09/ArithmeticExeptionTest.java
+++ src/main/java/com/oscarmorton/ejer09/ArithmeticExeptionTest.java
@@ -1,19 +1,9 @@
 package com.oscarmorton.ejer09;
 
-public class ArithmeticExeptionTest extends Ejer09 {
+public class ArithmeticExeptionTest extends ArithmeticException{
 
 
-    public ArithmeticExeptionTest(int[] arrayPruebaInts){
-        int elemento;
-
-        for(int i = 0; i < arrayPruebaInts.length; i++){
-            try {
-                elemento = 0 / arrayPruebaInts[i];
-
-            }catch (ArithmeticException ae){
-                System.out.println("Arithmetic Exeption en la posicion " + arrayPruebaInts[i] + "! No se puede dividir por 0");
-
-            }
-        }
+    public ArithmeticExeptionTest(String message) {
+        super(message);
     }
 }
